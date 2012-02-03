1193866630c936a64b93b842563b3039ee7de012
Sean Grove <s@bushi.do>
2012-02-02 17:45:14 -0800

    Adds the catch_all event. If an observer responds to :catch_all, and not a given event, we'll fire the catch_all method. If both are implemented in an observer, only the named event will be fired.

--- lib/bushido/data.rb
+++ lib/bushido/data.rb
@@ -11,11 +11,15 @@ module Bushido
       puts "Bushido Hooks Firing #{event} with => #{data.inspect}"
       @@observers.each do |observer|
         puts "#{observer}.respond_to?(#{event}) => #{observer.respond_to?(event)}"
-        if observer.respond_to?(event)
+        if observer.respond_to?(event) || observer.respond_to?(:catch_all)
           # Make a copy of the data so it's not mutated as the events
           # pass through the observers
           observer.instance_variable_set("@params", data.dup)
-          observer.send(event)
+          if observer.respond_to?(event)            
+            observer.send(event)
+          else
+            observer.send(:catch_all)
+          end
         end
       end
     end
