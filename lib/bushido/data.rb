ec71fba5540612e5d85c68378215ee4dfe930a1b
Sean Grove <s@bushi.do>
2012-02-04 16:16:28 -0800

    Uses a more careful algorithm for firing events, allows for an observer to stop propagation of an event, and calls the first catch_all hook it can find if no observer responds to the named event

--- lib/bushido/data.rb
+++ lib/bushido/data.rb
@@ -9,19 +9,44 @@ module Bushido
     
     def self.fire(data, event)
       puts "Bushido Hooks Firing #{event} with => #{data.inspect}"
+
+      processed = false
+
       @@observers.each do |observer|
         puts "#{observer}.respond_to?(#{event}) => #{observer.respond_to?(event)}"
-        if observer.respond_to?(event) || observer.respond_to?(:catch_all)
+
+        if observer.respond_to?(event)
+          processed = true
+
           # Make a copy of the data so it's not mutated as the events
           # pass through the observers
           observer.instance_variable_set("@params", data.dup)
-          if observer.respond_to?(event)            
-            observer.send(event)
-          else
+
+          result = observer.send(event)
+
+          # Allow an observer to halt event propagation
+          if result == :halt
+            puts "Observer #{observer} halted event propagation"
+            break
+          end
+        end
+      end
+
+      # We've checked all the observers to see if they respond to the
+      # named events, so if the event is still unprocessed then let's
+      # fall back on the first catch_all event we find
+      if !processed
+        @@observers.each do |observer|
+          if observer.respond_to?(:catch_all)
+            observer.instance_variable_set("@params", data.dup)
+
             observer.send(:catch_all)
+            break
           end
         end
       end
     end
   end
 end
+
+
