049192b2aed0e80c725a10335f0577ef4693833b
Sean Grove <s@bushi.do>
2012-06-02 18:52:20 -0700

    Trims the event API

--- lib/cloudfuji/event.rb
+++ lib/cloudfuji/event.rb
@@ -6,37 +6,23 @@ module Cloudfuji
   # * data
   # Data will hold the arbitrary data for the type of event signalled
   class Event
-    begin
-      @@events = JSON.parse(ENV["CLOUDFUJI_EVENTS"]) #:nodoc:
-    rescue
-      @@events = []
-    end
-
     attr_reader :category, :name, :data
 
     class << self
       def events_url
         "#{Cloudfuji::Platform.host}/apps/#{Cloudfuji::Platform.name}/events.json"
       end
+
+      def event_url(event_ido_id)
+        "#{Cloudfuji::Platform.host}/apps/#{Cloudfuji::Platform.name}/events/#{event_ido_id}.json"
+      end
       
-      # Lists all events
-      def all
-        @@events.collect{ |e| Event.new(e) }
-      end
-
-      # Lists the first (oldest) event
-      def first
-        Event.new(@@events.first)
-      end
-
-      # Lists the last (newest) event
-      def last
-        Event.new(@@events.last)
-      end
-
-      # NOOP right now
-      def refresh
-        @@events = Cloudfuji::Command.get_command(events_url)
+      # Find an event by its ido_id Be careful not to abuse this - an
+      # app can be throttled if requesting too many events too
+      # quickly, which will cause errors and a bad user experience for
+      # the end user
+      def find(event_ido_id)
+        Cloudfuji::Command.get_command(event_url(event_ido_id))
       end
 
       def publish(options={})
